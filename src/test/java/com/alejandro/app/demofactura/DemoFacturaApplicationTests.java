088f28b3eb0a2c2f5d475960d15c9dd6ac362a68
Alejandro9908 <joc.alejandro99@gmail.com>
2024-01-28 18:53:22 -0600

    first commit

---
+++ src/test/java/com/alejandro/app/demofactura/DemoFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.app.demofactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFacturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
